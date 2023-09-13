f982660a851a3887958a3333f5c9942e6136912e
guangdashao <54162228+guangdashao@users.noreply.github.com>
2023-09-13 14:48:56 +0800

    fix sms send error

--- powerjob-server/powerjob-server-core/src/main/java/tech/powerjob/server/core/alarm/dto/SmsJwtMessagePojo.java
+++ powerjob-server/powerjob-server-core/src/main/java/tech/powerjob/server/core/alarm/dto/SmsJwtMessagePojo.java
@@ -1,15 +1,16 @@
 package tech.powerjob.server.core.alarm.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
 /**
  * @author psychenDa
  * @description：
  * @date 2023/9/12 21:26
  **/
+@Data
+@AllArgsConstructor
 public class SmsJwtMessagePojo {
-    public SmsJwtMessagePojo(String jwt) {
-        this.jwt = jwt;
-    }
-
     /**
      * SmsMessageEntity 的jwt字符串
      */
